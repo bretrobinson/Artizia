ffefecce810ff2dd06daf7ba1ec59a034e830e7f
Jason Collins <jason.collins202009@gmail.com>
2021-01-10 12:55:53 -0700

    Create search bar

---
+++ user/components/SearchBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import Colors from './Colors';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.searchStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search for items"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchStyle: {
+    marginTop: 10,
+    // backgroundColor: '#F0EEEE',
+    borderColor: Colors.accent,
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
+
+export default SearchBar;
---
+++ user/screens/Landing.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const Landing = () => {
+    const [term, setTerm] = useState('');
+
+    return (
+          <View>
+            <SearchBar
+              term={term}
+              onTermChange={newTerm => setTerm(newTerm)}
+              onTermSubmit={() => console.log('term was submitted')}
+            />
+          </View>
+    );
+
+
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Landing;
