0f780601a7dd0967b430f988f39dc57c5f9952c8
Peter Oyebanji <peter@prob.ca>
2021-01-12 11:02:02 -0700

    signup signin

--- server.js
+++ server.js
@@ -1,12 +1,90 @@
 const express = require ('express')
 const bcrypt = require ('bcrypt')
+const jwt = require('jsonwebtoken')
+const requireAuth = require('./serverHanhlers/requireAuth')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.status(200).send('Server running')
+const database = {
+    users:[{
+        id: '120',
+        email: 'sally@gmail.com',
+        password: '1234',
+        joined: new Date()
+    }]
+}
+const requireAuthL = (req, res, next) => {
+    const {authorization} = req.headers
+    
+    if(!authorization) {
+        return res.status(401).send({error: 'You must be logged in'})
+    }
+    const token = authorization.replace('Bearer ', '' )
+    jwt.verify(token, 'MY_SECRETE_KEY', async (err, payload)=>{
+        if(err){
+            return res.status(401).send({error: 'You must be logged in'})
+        }
+        const {userId} = payload
+        
+        const user = await database.users.find(user => user.id=== userId)
+        req.user = user
+
+        next()
+    })
+}
+
+app.get('/', requireAuthL, (req, res)=>{
+    res.send(`Your email: ${req.user.email}`)
+})
+
+app.post('/signup', (req, res)=>{
+    const {password, email} = req.body
+    if (!email || !password){
+        return res.status(422).send('incorrect form submission')
+    }
+    try {
+        database.users.push({
+            id: `${database.users.length + 130}`,
+            email,
+            password,
+            joined: new Date()
+        })
+        const token = jwt.sign({userId: database.users[database.users.length-1].id}, 'MY_SECRETE_KEY')
+        // res.send(database.users[database.users.length-1])
+        res.send({id: database.users[database.users.length-1].id, token} )
+
+    } catch (err){
+        return res.status(422).send(err.message)
+    }
+    
+})
+
+app.post('/signin', (req, res)=>{
+    const {password, email} = req.body
+    console.log(email, password)
+    if (!email || !password){
+        return res.status(422).send('incorrect form submission')
+    }
+    let found = false 
+    database.users.forEach((user,i)=>{
+        console.log(user.email)
+        if(user.email === email){
+            if(user.password == password){
+                
+                found = true
+                if (found){
+                    const token = jwt.sign({userId: database.users.id}, 'MY_SECRETE_KEY')
+                    res.status(200).send(token)
+                }
+            }
+        }
+    })
+    if(!found) {
+        res.status(422).send('incorrect form submission')
+    }
+    
 })
 
 app.listen(3000, ()=>{
