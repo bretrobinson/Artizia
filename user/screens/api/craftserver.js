893528cc923faac7e42fac9fbd4d9598e8df0bf8
Peter Oyebanji <peter@prob.ca>
2021-01-12 11:18:25 -0700

    axios

---
+++ user/screens/api/craftserver.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://831f91f1be09.ngrok.io'
+})
+
+
+// instance.interceptors.request.use(
+//     async (config) => {
+//       const token = await AsyncStorage.getItem('token');
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//     },
+//     (err) => {
+//       return Promise.reject(err);
+//     }
+//   );
+  
+  export default instance;
