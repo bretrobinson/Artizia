8248f368d0fd762d22736e55be72ec904fecd753
Bret Robinson <kakadu.gm@gmail.com>
2021-01-22 16:01:07 -0700

    added item for addcategory

--- admin/src/App.js
+++ admin/src/App.js
@@ -12,56 +12,55 @@ import AnnouncementDisplay from './components/AnnouncementDisplay'
 import Announcement from './components/Announcement'
 import GeneralRules from './components/GeneralRules'
 import AnnouncementEdit from './components/AnnouncementEdit'
-
-
+import AddCategory from './components/AddCategory';
 
 function App() {
-const [title, setTitle] = useState('')
-const [message, setMessage] = useState('')
-const [expiredDate, setExpiredDate] = useState(new Date())
-const [annnouncementData, setAnnouncementData] = useState([])
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+  const [expiredDate, setExpiredDate] = useState(new Date())
+  const [annnouncementData, setAnnouncementData] = useState([])
+  const [category, setCategory] = useState();
 
-useEffect (()=>{
-loadAnnouncement()
-}, [])
+  useEffect (()=>{
+  loadAnnouncement()
+  }, [])
 
-const onSubmitmessage = async ({title, message, expiredDate})=>{
-if (!title|| !message ){
-    alert('Please enter Title , Message and Expired Date')
-}else {
-    try{
+  const onSubmitmessage = async ({title, message, expiredDate})=>{
+    if (!title|| !message ){
+      alert('Please enter Title , Message and Expired Date')
+    } else {
+      try{
         const response = await craftserverApi.post('/announcement', {title, message, expiredDate})
         await console.log(response.data)
         await loadAnnouncement()
         // await setMessage('')
- 
-    } catch (err) {
+      } catch (err) {
         console.log(err)
+      }
     }
-}
+  }
 
-}
+  const loadAnnouncement = async () =>{
+    try{
+      const response = await craftserverApi.get('/announcement')
+      await console.log(response.data)
+      setAnnouncementData(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-const loadAnnouncement = async () =>{
-try{
-    const response = await craftserverApi.get('/announcement')
-    await console.log(response.data)
-    setAnnouncementData(response.data)
-} catch (err) {
-    console.log(err)
-}
-}
-const onUpdateMessage = async (idMessage, message) =>{
-console.log(message)
-await craftserverApi.post('/announcement/' + idMessage ,{message})
-loadAnnouncement()
-}
+  const onUpdateMessage = async (idMessage, message) =>{
+    console.log(message)
+    await craftserverApi.post('/announcement/' + idMessage ,{message})
+    loadAnnouncement()
+  }
 
 
-const onDeleteMessage = async (idMessage) =>{
- await craftserverApi.delete('/announcement/' + idMessage)
-loadAnnouncement()
-}
+  const onDeleteMessage = async (idMessage) => {
+    await craftserverApi.delete('/announcement/' + idMessage)
+    loadAnnouncement()
+  }
 
   return (
 
@@ -82,6 +81,9 @@ loadAnnouncement()
             <li>
               <Link to="/rules">Rules</Link>
             </li>
+            <li>
+              <Link to="/addCategory">Add Category</Link>
+            </li>
           </ul>
         </nav>
 
@@ -89,48 +91,47 @@ loadAnnouncement()
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/message">
-          <Announcement onChangeTitle={setTitle}
-      onChangeExpiredDate={setExpiredDate}
-      onChangeMessage={setMessage} 
-      onSubmitmessage={()=>onSubmitmessage({title, message, expiredDate})}
-      title={title} message={message} expiredDate={expiredDate}
-      
-       />
+            <Announcement onChangeTitle={setTitle}
+              onChangeExpiredDate={setExpiredDate}
+              onChangeMessage={setMessage} 
+              onSubmitmessage={()=>onSubmitmessage({title, message, expiredDate})}
+              title={title} message={message} expiredDate={expiredDate}
+            />
           </Route>
           <Route path="/edit/:idAnnouncements">
-          <AnnouncementEditPage onChangeTitle={setTitle}
-      onChangeExpiredDate={setExpiredDate}
-      onChangeMessage={setMessage} 
-      onUpdateMessage={onUpdateMessage}
-      title={title} message={message} expiredDate={expiredDate}
-       
-       />
+            <AnnouncementEditPage onChangeTitle={setTitle}
+              onChangeExpiredDate={setExpiredDate}
+              onChangeMessage={setMessage} 
+              onUpdateMessage={onUpdateMessage}
+              title={title} message={message} expiredDate={expiredDate}
+            />
           </Route>
           <Route path="/rules">
-          <GeneralRules />
+            <GeneralRules />
           </Route>
+          <Route path="/addCategory" component={AddCategory} />
           <Route path="/">
-          <AnnouncementDisplay annnouncementData={annnouncementData}
-         onDeleteMessage={onDeleteMessage} message={message}
-        />
+            <AnnouncementDisplay annnouncementData={annnouncementData}
+              onDeleteMessage={onDeleteMessage} message={message}
+            />
           </Route>
         </Switch>
       </div>
     </Router>
-        
-     
     </div>
   );
 }
+
 const AnnouncementEditPage =({onChangeTitle, onChangeExpiredDate, onChangeMessage, onUpdateMessage, title})=>{
 let {idAnnouncements} = useParams()
 console.log(idAnnouncements)
 return <AnnouncementEdit idAnnouncements={idAnnouncements} 
-onChangeTitle={onChangeTitle} 
-onChangeExpiredDate={onChangeExpiredDate} 
-onChangeMessage={onChangeMessage}
-onUpdateMessage={onUpdateMessage}
-title={title} />
+    onChangeTitle={onChangeTitle} 
+    onChangeExpiredDate={onChangeExpiredDate} 
+    onChangeMessage={onChangeMessage}
+    onUpdateMessage={onUpdateMessage}
+    title={title}
+  />
 }
 
 export default App;
