3331b66fc16b8e00aac129fc8fccdecf99354ab8
ROGER Trewin <rogertrewin.r@gmail.com>
2021-02-21 14:10:48 -0700

    update files

--- user/api/craftserver.js
+++ user/api/craftserver.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-    baseURL: 'http://be9d2bb0ef69.ngrok.io',
+    baseURL: 'http://8cbf689c3a1e.ngrok.io',
    })
 
 instance.interceptors.request.use(
