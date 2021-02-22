4c3ececf116d3d49a98e29cb9a16b9430f6f7f02
Jason Collins <jason.collins202009@gmail.com>
2021-02-22 16:41:36 -0700

    Update user api url.

--- user/api/craftserver.js
+++ user/api/craftserver.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-    baseURL: 'http://fee584d600ca.ngrok.io',
+    baseURL: 'https://artizia.herokuapp.com',
    })
 
 instance.interceptors.request.use(
