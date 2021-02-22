0603fd3094acfac4ad4a8afa41e84dde983293d1
Bret Robinson <kakadu.gm@gmail.com>
2021-02-22 08:42:01 -0700

    ngrok key

--- user/api/craftserver.js
+++ user/api/craftserver.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://073525f85744.ngrok.io',
-=======
     baseURL: 'http://4ff181127b2f.ngrok.io',
->>>>>>> 7ceae87c97a2b71f3d82885e3de9df44d7ef17ae
    })
 
 instance.interceptors.request.use(
